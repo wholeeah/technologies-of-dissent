274992a4e870109db0a0380fd141ecba3b89003d
wholeeah <34227054+wholeeah@users.noreply.github.com>
2017-12-06 15:39:22 -0500

    Create outreach-meeting-one-notes.m

---
+++ outreach-meeting-one-notes.m
@@ -0,0 +1,14 @@
+Framing Questions
+Interview questions for Public Safety employees that have been working for a long time- 
+how have their jobs changed as new surveillance tehnologies have been introduced
+Do they recieve training when new technologies are introduced?
+How long do they keep the surveillance tapes- who has access to view and store them?
+What is the protocol when you see suspicious behavior on the cameras
+Interview style questions vs official response
+CU IT- Does public safety have their own IT or do they go through their central IT
+Where is the person watching CCTV in relation to the area being surveilled? What is the protocol of the person watching?
+In what situations would they be required to call NYPD?
+Barnard vs Columbia public safety
+Katie: Email Columbia and Barnard Public Safety Communications
+Public Safety report
+Julia- reach out to employees
